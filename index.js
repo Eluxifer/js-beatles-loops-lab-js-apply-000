25ad1b1329c69c96b7a4966374d17f8b5cb837ca
Jeison Rodriguez <jeisonrodriguez94@outlook.com>
2018-11-27 20:13:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var empty = [];
   for (let i = 0; i < 4; i++) {
-    empty.push(`${musicians[0]} plays ${instruments[0]}`);
+    empty.push(`${musicians[i]} plays ${instruments[i]}`);
   }
   return empty;
 }
