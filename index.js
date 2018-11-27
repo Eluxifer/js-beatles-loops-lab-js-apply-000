92fa631d76aa2799ebf199164c55d961918b73d4
Jeison Rodriguez <jeisonrodriguez94@outlook.com>
2018-11-27 20:25:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function theBeatlesPlay(musicians, instruments) {
   }
   return empty;
 }
-/*const facts = [
+const facts = [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
@@ -18,4 +18,4 @@ function johnLennonFacts(facts){
     facts[n] + "!!!";
   }
   return facts;
-}*/
+}
