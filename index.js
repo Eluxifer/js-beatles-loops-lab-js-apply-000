3644ed0308da080c7a763ec1a19857eae0643201
Jeison Rodriguez <jeisonrodriguez94@outlook.com>
2018-11-27 20:20:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
-  var empty = []
-  for (let i = 0 i < 4 i++) {
-    empty.push(`${musicians[i]} plays ${instruments[i]}`)
+  var empty = [];
+  for (let i = 0; i < 4; i++) {
+    empty.push(`${musicians[i]} plays ${instruments[i]}`);
   }
   return empty;
 }
-function johnLennonFacts(){
-  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"]
-     var newFacts = []
-     var i = 0
+function johnLennonFacts(facts){
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+     var newFacts = [];
+     var i = 0;
      while (i < facts.length) {
-         newFacts.push(facts[i] +"!!!")
-         i++
+         newFacts.push(facts[i] +"!!!");
+         i++;
      }
-     return newFacts
+     return newFacts;
 }
