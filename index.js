fe3e953874122d87549babb1b8b483e74e94ce47
Jeison Rodriguez <jeisonrodriguez94@outlook.com>
2018-11-27 20:05:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var empty = [];
+  for (var i=0; i < 2; i++) {
+    empty.push(`${musicians[0]} plays ${instruments[0]}`);
+  }
+  return empty;
+}
+
